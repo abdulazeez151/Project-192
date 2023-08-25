69500719cc74469b27d8d7f4f4648f566fa5c9ac
abdulazeez151 <74958650+abdulazeez151@users.noreply.github.com>
2023-08-25 16:52:09 +0530

    Add files via upload

---
+++ playerMovement.js
@@ -0,0 +1,19 @@
+AFRAME.registerComponent("player-movement", {
+    init: function () {
+      this.move();
+    },
+    move: function () {
+      window.addEventListener("keydown", (e) => {
+        if (
+          e.key === "ArrowUp" ||
+          e.key === "ArrowRight" ||
+          e.key === "ArrowLeft" ||
+          e.key === "ArrowDown"
+        ) {
+          var entity = document.querySelector("#sound2");
+          entity.components.sound.playSound();
+        }
+      });
+    },
+  });
+  
